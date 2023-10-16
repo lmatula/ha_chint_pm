fff2d4f4863b0c29c7942efb3119994485dced3a
lmatula <lajos.matula@gmail.com>
2023-10-16 19:37:55 +0200

    Update const.py

--- custom_components/chint_pm/const.py
+++ custom_components/chint_pm/const.py
@@ -1,4 +1,18 @@
 """Constants for the Chint pm integration."""
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorStateClass,
+)
+from homeassistant.const import (
+    ENERGY_KILO_WATT_HOUR,
+    POWER_VOLT_AMPERE_REACTIVE,
+    POWER_WATT,
+    UnitOfElectricCurrent,
+    UnitOfElectricPotential,
+    UnitOfFrequency,
+)
+from homeassistant.helpers.entity import EntityCategory
+
 from datetime import timedelta
 
 DOMAIN = "chint_pm"
@@ -10,10 +24,16 @@ DEFAULT_PASSWORD = ""
 
 CONF_SLAVE_IDS = "slave_ids"
 CONF_PHASE_MODE = "phase_mode"
+CONF_METER_TYPE = "meter_type"
 
 DATA_UPDATE_COORDINATORS = "update_coordinators"
 
-UPDATE_INTERVAL = timedelta(seconds=30)
+UPDATE_INTERVAL = timedelta(seconds=15)
 
 PHMODE_3P4W = "3P4W"
 PHMODE_3P3W = "3P3W"
+
+
+class MeterTypes:
+    METER_TYPE_H_3P = "1"
+    METER_TYPE_CT_3P = "2"
